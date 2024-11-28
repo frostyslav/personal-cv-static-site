a8dad76f84fe4d8f7cf194a382d0989d0df1e451
frostyslav <rostyslav.fridman@gmail.com>
2024-11-28 17:35:22 +0100

    feat: format files

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,8 +1,8 @@
-$('a').click(function(){
-    console.log($(this).text());
-    if($(this).text().trim() == 'Expand' ){
-        $(this).text('Collapse');
-    }else{
-         $(this).text('Expand');
-    }
+$("a").click(function () {
+  console.log($(this).text());
+  if ($(this).text().trim() == "Expand") {
+    $(this).text("Collapse");
+  } else {
+    $(this).text("Expand");
+  }
 });
